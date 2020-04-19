3357891fdc33251de7cb001edbb933045e637c57
ayser259 <ayserchoudhury@gmail.com>
2020-04-19 16:07:44 -0400

    updating settings

--- settings.py
+++ settings.py
@@ -127,5 +127,5 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
