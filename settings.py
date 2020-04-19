642fd05ea071f7c0cb71f66dd16b47ed91f688c7
ayser259 <ayserchoudhury@gmail.com>
2020-04-19 16:05:11 -0400

    updating settings

--- settings.py
+++ settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/website/'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
